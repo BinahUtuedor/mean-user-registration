ca86126b36e6aacfd3325c483f578ee31112fef5
Binah Utuedor <doctorbinah@yahoo.com>
2020-12-15 22:21:05 +0100

    added controllers and routes

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // Check env.
 var env = process.env.NODE_ENV || 'development';
-// Fetch env. config
+// fetch env. config
 var config = require('./config.json');
 var envConfig = config[env];
 // add env.config values to process.env
